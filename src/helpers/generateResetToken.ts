8cdb122ddb9faf26927f67b7689248580fc1248d
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-06-16 16:01:48 +0545

    added generate reset token and send reset link with email

---
+++ src/helpers/generateResetToken.ts
@@ -0,0 +1,30 @@
+import {v4 as uuidv4} from 'uuid'
+import logger from '../utils/logger.utils'
+
+interface GenTokenStatus {
+    success: boolean,
+    message: string,
+    token?: string
+}
+
+// function to generate random uuid
+const generateResetToken = (): GenTokenStatus  => {
+    try {
+        // generate uuid token
+        const resetToken = uuidv4()
+
+        return {
+            success: true,
+            message: 'Successfully generated reset token',
+            token: resetToken
+        }
+    } catch (err) {
+        logger.error('Error while generting reset token', err)
+        return {
+            success: false,
+            message: 'Error while genrating reset token'
+        }
+    }
+}
+
+export default generateResetToken
---
+++ src/helpers/sendResetEmail.ts
@@ -0,0 +1,49 @@
+import sgMail, { type MailDataRequired } from '@sendgrid/mail'
+import * as dotenv from 'dotenv'
+import logger from '../utils/logger.utils'
+
+dotenv.config()
+
+export interface SendResetEmailStatus {
+    success: boolean
+    message: string
+}
+
+// sendgrid api
+const SENDGRID_API: string = String(process.env.SENDGRID_API)
+
+// config sendgrid api
+sgMail.setApiKey(SENDGRID_API)
+
+// function to send reset email
+const sendResetEmail = async (receiverEmail: string, token: string): Promise<SendResetEmailStatus> => {
+    try {
+
+        const resetLink: string = `http://localhost:3000/reset-password/${token} `
+
+        const msgOptions: MailDataRequired = {
+            to: receiverEmail,
+            from: String(process.env.SENDGRID_DEFAULT_EMAIL),
+            subject: 'PASSWORD RESET',
+            text: 'here you can reset your password',
+            html: `<p>You requested a password reset so please visit <a href=${resetLink} /> and note it that it is only valid for 1 hour only. Thank You</p>`
+        }
+
+        // send email
+        await sgMail.send(msgOptions)
+
+        return {
+            success: true,
+            message: 'Successfully sent reset email'
+        }
+
+    } catch (err) {
+        logger.error('Error while sending reset email', err)
+        return {
+            success: false,
+            message: 'Error while sending reset email'
+        }
+    }
+}
+
+export default sendResetEmail
