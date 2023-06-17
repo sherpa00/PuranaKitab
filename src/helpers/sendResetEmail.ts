ed19e5644160ee11b03aa006cf7588d93abf71c8
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-06-17 13:26:26 +0545

    removed google auth for sending email

--- src/helpers/sendResetEmail.ts
+++ src/helpers/sendResetEmail.ts
@@ -1,5 +1,5 @@
 
-import nodemailer, { type Transporter } from 'nodemailer'
+
 import * as dotenv from 'dotenv'
 import logger from '../utils/logger.utils'
 
@@ -11,30 +11,13 @@ export interface SendResetEmailStatus {
 }
 
 // function to send reset email
-const sendResetEmail = async (receiverEmail: string, token: string): Promise<SendResetEmailStatus> => {
+const sendResetEmail = async (receiverEmail: string, resetToken: string): Promise<SendResetEmailStatus> => {
     try {
 
-        const resetLink: string = `http://localhost:3003/forgot-password/${token}`
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const resetLink: string = `http://localhost:3003/forgot-password/${resetToken}`
 
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-              user: process.env.DEFAULT_EMAIL,
-              pass: process.env.DEFAULT_EMAIL_PASSWORD,
-            },
-          });
 
-        const msgOptions = {
-            from: process.env.DEFAULT_EMAIL,
-            to: receiverEmail,
-            subject: 'PASSWORD RESET',
-            text: 'Sending reset password link',
-            html: `You requested reset passoword so please visit ${resetLink} to reset password.`
-        }
-
-        // send email
-        await transporter.sendMail(msgOptions)
-        
         return {
             success: true,
             message: 'Successfully sent reset email'
