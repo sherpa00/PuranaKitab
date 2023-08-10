22bef0c187c1f5c2d28878ba0f4c4e9dc90a50a3
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-08-10 23:03:10 +0545

    added unit test for cart services

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -8,24 +8,23 @@ const GetAllCart = async (userID: number): Promise<ServiceResponse> => {
     // get cart from userid
     const getCartStatus = await db.query('SELECT * FROM carts WHERE carts.userid = $1', [userID])
 
-    if (getCartStatus.rowCount <= 0) {
+    if (getCartStatus.rowCount < 0) {
       return {
         success: true,
-        message: 'Empty Cart',
-        data: []
+        message: 'Failed to get all carts'
       }
     }
 
     return {
       success: true,
-      message: 'Successfully got all cart',
+      message: 'Successfully got all carts',
       data: getCartStatus.rows
     }
   } catch (err) {
-    logger.error(err, 'Error while gettting all cart')
+    logger.error(err, 'Error while gettting all carts')
     return {
       success: false,
-      message: 'Error while gettting all cart'
+      message: 'Failed to get all carts'
     }
   }
 }
@@ -34,7 +33,7 @@ const GetAllCart = async (userID: number): Promise<ServiceResponse> => {
 const AddCart = async (userID: number, bookID: number, bookQuantity: number): Promise<ServiceResponse> => {
   try {
     // get the book from db by bookid
-    const foundBookWithBookid = await db.query('SELECT * FROM books WHERE bookid = $1', [bookID])
+    const foundBookWithBookid = await db.query('SELECT price, available_quantity FROM books WHERE bookid = $1', [bookID])
 
     if (foundBookWithBookid.rowCount <= 0) {
       return {
@@ -69,20 +68,20 @@ const AddCart = async (userID: number, bookID: number, bookQuantity: number): Pr
     if (addNewCartStatus.rowCount <= 0) {
       return {
         success: false,
-        message: 'Failed to add new cart'
+        message: 'Failed to add cart'
       }
     }
 
     return {
       success: true,
-      message: 'Succesfully added cart',
+      message: 'Successfully added cart',
       data: addNewCartStatus.rows[0]
     }
   } catch (err) {
     logger.error(err, 'Error while adding new cart')
     return {
       success: false,
-      message: 'Error while adding new cart'
+      message: 'Failed to add cart'
     }
   }
 }
@@ -91,7 +90,7 @@ const AddCart = async (userID: number, bookID: number, bookQuantity: number): Pr
 const UpdateCart = async (userID: number, cartID: number, bookQuantity: number): Promise<ServiceResponse> => {
   try {
     // first verify if cart exits or not with cartID
-    const foundCart = await db.query('SELECT * FROM carts WHERE carts.cartid = $1 AND carts.userid = $2', [
+    const foundCart = await db.query('SELECT bookid, original_price FROM carts WHERE carts.cartid = $1 AND carts.userid = $2', [
       cartID,
       userID
     ])
@@ -138,20 +137,20 @@ const UpdateCart = async (userID: number, cartID: number, bookQuantity: number):
     if (updateCartStatus.rowCount <= 0) {
       return {
         success: false,
-        message: 'Failed to update a cart'
+        message: 'Failed to update cart'
       }
     }
 
     return {
       success: true,
-      message: 'Successfully updated a cart',
+      message: 'Successfully updated cart',
       data: updateCartStatus.rows[0]
     }
   } catch (err) {
     logger.error(err, 'Error while updating a cart')
     return {
       success: false,
-      message: 'Error while updating a cart'
+      message: 'Failed to update cart'
     }
   }
 }
@@ -160,7 +159,7 @@ const UpdateCart = async (userID: number, cartID: number, bookQuantity: number):
 const RemoveSingleCart = async (userID: number, cartID: number): Promise<ServiceResponse> => {
   try {
     // verify if cart exists or not
-    const foundCart = await db.query('SELECT * FROM carts WHERE carts.cartid = $1 AND carts.userid = $2', [
+    const foundCart = await db.query('SELECT cartid FROM carts WHERE carts.cartid = $1 AND carts.userid = $2', [
       cartID,
       userID
     ])
@@ -173,7 +172,7 @@ const RemoveSingleCart = async (userID: number, cartID: number): Promise<Service
     }
 
     // remove cart
-    const removeSingleCartStatus = await db.query('DELETE FROM carts WHERE carts.cartid = $1 AND carts.userid = $2', [
+    const removeSingleCartStatus = await db.query('DELETE FROM carts WHERE carts.cartid = $1 AND carts.userid = $2 RETURNING *', [
       cartID,
       userID
     ])
@@ -181,19 +180,20 @@ const RemoveSingleCart = async (userID: number, cartID: number): Promise<Service
     if (removeSingleCartStatus.rowCount <= 0) {
       return {
         success: false,
-        message: 'Failed to remove a single cart'
+        message: 'Failed to remove cart'
       }
     }
 
     return {
       success: true,
-      message: 'Successfully removed a cart'
+      message: 'Successfully removed cart',
+      data: removeSingleCartStatus.rows[0]
     }
   } catch (err) {
     logger.error(err, 'Error while removing a single cart')
     return {
       success: false,
-      message: 'Error while removing a cart'
+      message: 'Failed to remove cart'
     }
   }
 }
@@ -202,7 +202,7 @@ const RemoveSingleCart = async (userID: number, cartID: number): Promise<Service
 const RemoveAllCart = async (userID: number): Promise<ServiceResponse> => {
   try {
     // verify if cart exists or not
-    const foundCart = await db.query('SELECT * FROM carts WHERE carts.userid = $1', [userID])
+    const foundCart = await db.query('SELECT cartid FROM carts WHERE carts.userid = $1', [userID])
 
     if (foundCart.rowCount <= 0) {
       return {
@@ -212,7 +212,7 @@ const RemoveAllCart = async (userID: number): Promise<ServiceResponse> => {
     }
 
     // remove cart
-    const removeAllCartStatus = await db.query('DELETE FROM carts WHERE carts.userid = $1', [userID])
+    const removeAllCartStatus = await db.query('DELETE FROM carts WHERE carts.userid = $1 RETURNING *', [userID])
 
     if (removeAllCartStatus.rowCount <= 0) {
       return {
@@ -223,13 +223,14 @@ const RemoveAllCart = async (userID: number): Promise<ServiceResponse> => {
 
     return {
       success: true,
-      message: 'Successfully removed all carts'
+      message: 'Successfully removed all carts',
+      data: removeAllCartStatus.rows
     }
   } catch (err) {
     logger.error(err, 'Error while removing all carts')
     return {
       success: false,
-      message: 'Error while removing all carts'
+      message: 'Failed to remove all carts'
     }
   }
 }
